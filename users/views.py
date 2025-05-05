99ce41a5fd83c61e1e32509a2f963e66163461bd
anor23 <burgchiken@gmail.com>
2025-05-05 21:12:42 +0300

    переименован файл написаны энд поинты и полный круд

--- users/views.py
+++ users/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
-
+from rest_framework.generics import ListAPIView, CreateAPIView , DestroyAPIView , RetrieveAPIView, UpdateAPIView
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import IsAuthenticated , AllowAny
 # Create your views here.
+
+from users.serializers.user_serializers import UserCreateSerializer, UserUpdateSerializer , UserSerializer ,UserTokenObtainPairSerializer
+from users.models import  User
+
+class UserListApiView(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserCreateApiView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserCreateSerializer
+    permission_classes = (AllowAny,)
+
+class UserRetrieveAPIView(RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+class UserUpdateAPIView(UpdateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+
+    def get_queryset(self):
+        user = self.request.user
+        return User.objects.filter(id=user.id)
+
+class UserDestroyApiView(DestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+
+class UserTokenObtainPairView(TokenObtainPairView):
+    serializer_class = UserTokenObtainPairSerializer
+
+
+
+
