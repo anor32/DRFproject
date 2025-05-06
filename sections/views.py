c2cfb2f6bf0d813888a2cdbedeec25d6633f9769
anor23 <burgchiken@gmail.com>
2025-05-06 20:04:22 +0300

    написаны эндопониты cекций и контента

---
+++ sections/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.response import Response
+
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
+from sections.models import Section, Content
+from sections.permissions import IsModerator
+from sections.serializers.section_serializers import SectionSerializer, SectionListSerializer
+from sections.serializers.context_serializers import ContentSerializer, ContentListSerializer, \
+    ContentListSerializer
+
+from sections.paginators import SectionPaginator, SectionContentPaginator
+
+
+class SectionListApiView(ListAPIView):
+    serializer_class = SectionListSerializer
+    queryset = Section.objects.all()
+    permission_classes = (IsAuthenticated,)
+    permission_clas = SectionPaginator
+
+
+class SectionCreateAPIView(CreateAPIView):
+    serializer_class = SectionSerializer
+    permission_classes = (IsAuthenticated, IsAdminUser | IsModerator)
+
+
+class SectionRetrieveApiView(RetrieveAPIView):
+    serializer_class = SectionSerializer
+    queryset = Section.objects.all()
+    permission_classes = (IsAuthenticated,)
+
+
+class SectionUpdateAPIView(UpdateAPIView):
+    serializer_class = SectionSerializer
+    queryset = Section.objects.all()
+    permission_classes = (IsAuthenticated, IsAdminUser | IsModerator)
+
+
+class SectionDestroyAPIView(DestroyAPIView):
+    serializer_class = SectionSerializer
+    queryset = Section.objects.all()
+    permission_classes = (IsAuthenticated, IsAdminUser | IsModerator)
+
+#content
+class ContentListApiView(ListAPIView):
+    serializer_class = ContentSerializer
+    queryset = Content.objects.all()
+    permission_classes = (IsAuthenticated,)
+    pagination_class = SectionContentPaginator
+
+
+class ContentRetrieveApiView(RetrieveAPIView):
+    serializer_class = ContentSerializer
+    queryset = Content.objects.all()
+    permission_classes = (IsAuthenticated,)
+
+
+class ContentCreateApiView(RetrieveAPIView):
+    serializer_class = ContentSerializer
+    queryset = Content.objects.all()
+    permission_classes = (IsAuthenticated,)
+
+
+class ContentUpdateAPIView(UpdateAPIView):
+    serializer_class =  ContentSerializer
+    queryset = Content.objects.all()
+    permission_classes = (IsAuthenticated, IsAdminUser | IsModerator)
+
+
+class ContentDestroyAPiView(DestroyAPIView):
+    serializer_class =  ContentSerializer
+    queryset = Content.objects.all()
+    permission_classes = (IsAuthenticated, IsAdminUser | IsModerator)
